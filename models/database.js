cc518b762b293c3e4073f089664e74c6d09d2743
kacy hulme <kacyhulme@gmail.com>
2015-06-24 11:25:16 -0500

    redid database to separate out question and options into two different tables

--- models/database.js
+++ models/database.js
@@ -3,9 +3,9 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/le
 var client = new pg.Client(connectionString);
 client.connect();
 
-//var query = client.query('CREATE TABLE polls(id SERIAL PRIMARY KEY, question VARCHAR(40) not null, option_one VARCHAR(40) not null, option_two VARCHAR(40) not null, option_three VARCHAR(40) not null, option_four VARCHAR(40) not null, votes INTEGER)');
+//var query = client.query('CREATE TABLE polls(poll_id SERIAL PRIMARY KEY, question VARCHAR(40) not null)');
 //query.on('end', function() { client.end(); });
 
-//var query = client.query('CREATE TABLE poll_options(id SERIAL PRIMARY KEY, option_text VARCHAR(100) not null, votes INTEGER)');
+//var query = client.query('CREATE TABLE poll_options(poll_option_id SERIAL PRIMARY KEY, poll_id integer REFERENCES polls (poll_id), option_text VARCHAR(100) not null, votes INTEGER)');
 //query.on('end', function() { client.end(); });
 
